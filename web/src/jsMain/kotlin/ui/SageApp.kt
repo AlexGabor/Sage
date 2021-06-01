78fe2a380cd22d163ce0821f5b8e67de30f74e19
Alex Gabor <alex.dan.gabor@gmail.com>
2021-06-01 14:47:44 +0300

    Fix background scroll

--- web/src/jsMain/kotlin/ui/SageApp.kt
+++ web/src/jsMain/kotlin/ui/SageApp.kt
@@ -1,18 +1,24 @@
 package ui
 
 import androidx.compose.runtime.Composable
-import org.jetbrains.compose.common.core.graphics.Color
+import androidx.compose.web.css.percent
+import androidx.compose.web.css.value
+import androidx.compose.web.elements.Div
 import org.jetbrains.compose.common.foundation.layout.Box
 import org.jetbrains.compose.common.foundation.layout.fillMaxHeight
 import org.jetbrains.compose.common.foundation.layout.fillMaxWidth
 import org.jetbrains.compose.common.ui.Modifier
-import org.jetbrains.compose.common.ui.background
 
 @Composable
 fun SageApp() {
-    Box(Modifier.fillMaxHeight(1f)
-        .fillMaxWidth()
-        .background(Color(0xe8, 0xdc, 0xd1))) {
-        Navigator()
+    Div(style = {
+        property("min-height", value(100.percent))
+        property("width", value(100.percent))
+        property("background-color", value("#e8dcd1"))
+    }) {
+        Box(Modifier.fillMaxHeight(1f)
+            .fillMaxWidth()) {
+            Navigator()
+        }
     }
 }
