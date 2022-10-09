1297f2b770457015f924d07dccd4b8013410b854
Alex Gabor <alex.dan.gabor@gmail.com>
2022-10-09 11:27:48 +0300

    Add viewport meta tag

--- kobweb/build.gradle.kts
+++ kobweb/build.gradle.kts
@@ -1,4 +1,5 @@
 import kotlinx.html.link
+import kotlinx.html.meta
 
 plugins {
     kotlin("multiplatform")
@@ -21,6 +22,7 @@ kobweb {
     index {
         description.set("Powered by Kobweb")
         head.set(listOf {
+            meta(name = "viewport", content = "width=device-width, initial-scale=1.0")
             link(rel = "stylesheet", href = "/root.css")
             link(rel = "preconnect", href = "https://fonts.googleapis.com")
             link(rel = "preconnect", href = "https://fonts.gstatic.com")
